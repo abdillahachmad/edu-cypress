535b4c0125b944484e4655226311e63f2fdde3d4
Hendrich <hendri.christianto24@gmail.com>
2024-11-03 19:57:29 +0700

    update cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,9 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    specPattern: [
+      "cypress/integrations/**/*.cy.{js,jsx,ts,tsx}",
+      "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+    ],
   },
 });
