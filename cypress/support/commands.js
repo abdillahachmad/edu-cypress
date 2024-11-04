47d99c79b12a748a4eee232746f30ee3cb3e6733
unknown <achmadabdillah20@gmail.com>
2024-11-04 21:59:31 +0700

    API Authentication

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,36 +24,54 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
-Cypress.Commands.add("login", (username, password) => {
-  cy.clearCookies();
-  cy.clearLocalStorage();
-  cy.get("#user_login").clear();
-  cy.get("#user_login").type(username);
+// Cypress.Commands.add("login", (username, password) => {
+//   cy.clearCookies();
+//   cy.clearLocalStorage();
+//   cy.get("#user_login").clear();
+//   cy.get("#user_login").type(username);
 
-  cy.get('input[name="user_password"]').clear();
-  cy.get('input[name="user_password"]').type(password);
+//   cy.get('input[name="user_password"]').clear();
+//   cy.get('input[name="user_password"]').type(password);
 
-  cy.get('input[name="submit"]').click();
-});
+//   cy.get('input[name="submit"]').click();
+// });
 
-Cypress.Commands.add("makePayment", () => {
-  cy.fixture("payment").then((payment) => {
-    // Load data from the fixture
-    // Set the payee
-    cy.get("#sp_payee").should("be.visible").select(payment.payee);
-    // Set the account
-    cy.get("#sp_account").type(payment.account);
+// Cypress.Commands.add("makePayment", () => {
+//   cy.fixture("payment").then((payment) => {
+//     // Load data from the fixture
+//     // Set the payee
+//     cy.get("#sp_payee").should("be.visible").select(payment.payee);
+//     // Set the account
+//     cy.get("#sp_account").type(payment.account);
 
-    // Set the amount
-    cy.get("#sp_amount").type(payment.amount);
+//     // Set the amount
+//     cy.get("#sp_amount").type(payment.amount);
 
-    // Set the payment date
-    cy.get("#sp_date").type(payment.date);
+//     // Set the payment date
+//     cy.get("#sp_date").type(payment.date);
 
-    // Set the description
-    cy.get("#sp_description").type(payment.description, { force: true });
+//     // Set the description
+//     cy.get("#sp_description").type(payment.description, { force: true });
 
-    // Submit the form
-    cy.get("#pay_saved_payees").click();
-  });
-});
+//     // Submit the form
+//     cy.get("#pay_saved_payees").click();
+//   });
+// });
+
+Cypress.Commands.add(
+  "loginViaAPI",
+  (
+    email = Cypress.env("userEmail"),
+    password = Cypress.env("userPassword")
+  ) => {
+    cy.request("POST", `${Cypress.env("apiUrl")}/users/login`, {
+      username: email,
+      password,
+    }).then((response) => {
+      cy.setCookie("sessionId", response.body.sessionId);
+      cy.setCookie("userId", response.body.userId);
+      cy.setCookie("userName", response.body.userName);
+      cy.visit("/#!/main");
+    });
+  }
+);
